e491605ec51038a1499c8a80b135add638b8ed4d
jx-mzc <42129607+jx-mzc@users.noreply.github.com>
2018-10-31 12:13:45 +0800

    利用SharedPreferences存储实现记住密码功能

---
+++ LoginRemember/app/src/main/java/com/example/administrator/loginremember/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.administrator.loginremember;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private EditText accountEdit;
+    private EditText passwordEdit;
+    private Button login;
+    private CheckBox rememberPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        accountEdit = (EditText)findViewById(R.id.account);
+        passwordEdit = (EditText)findViewById(R.id.password);
+        rememberPass = (CheckBox)findViewById(R.id.remember_pass);
+        login = (Button)findViewById(R.id.login);
+        boolean isRemember = preferences.getBoolean("remember_password",false);
+        if (isRemember){
+            String account = preferences.getString("account","");
+            String password = preferences.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                if (account.equals("admin")&&password.equals("123456")){
+                    editor = preferences.edit();
+                    if (rememberPass.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else {
+                        editor.clear();
+                    }
+                    editor.apply();
+                    Toast.makeText(MainActivity.this,"登录成功",Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"登录失败",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
